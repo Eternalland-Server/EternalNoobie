49cfa5c5040a667ba7f0ce7d20d0e0aba8a8f0dd
kirraObj <c@skymc.me>
2022-05-18 16:49:24 +0800

    update.

--- src/main/kotlin/net/sakuragame/eternal/noobie/function/FunctionListener.kt
+++ src/main/kotlin/net/sakuragame/eternal/noobie/function/FunctionListener.kt
@@ -1,9 +1,7 @@
 package net.sakuragame.eternal.noobie.function
 
 import net.luckperms.api.node.types.PermissionNode
-import net.sakuragame.eternal.dragoncore.api.event.YamlSendFinishedEvent
 import net.sakuragame.eternal.justmessage.api.MessageAPI
-import net.sakuragame.eternal.justquest.JustQuest
 import net.sakuragame.eternal.justquest.api.JustQuestAPI
 import net.sakuragame.eternal.justquest.api.event.QuestAccountLoadEvent
 import net.sakuragame.eternal.noobie.EternalNoobie
@@ -23,20 +21,21 @@ object FunctionListener {
     fun e(e: QuestAccountLoadEvent) {
         submit(async = false, delay = 20L) {
             val player = e.player ?: return@submit
-            val account = e.account
+            val account = e.account ?: return@submit
             if (player.hasPermission(PERMISSION)) {
                 return@submit
             }
-            if (account.finished.contains(QUEST_ID) || account.questProgress.keys.contains(QUEST_ID)) {
+
+            if (account.finished.contains(QUEST_ID)) {
                 return@submit
             }
             JustQuestAPI.allotQuest(player, QUEST_ID)
             player.sendTitle("", "&7&o一百年后...".colored(), 10, 100, 10)
-            player.addPotionEffect(PotionEffect(PotionEffectType.CONFUSION, 99999999, 40, false))
-            submit(delay = 40) {
+            player.addPotionEffect(PotionEffect(PotionEffectType.BLINDNESS, 99999999, 40, false))
+            submit(async = false, delay = 40) {
                 player.sendTitle("", "&d&o诶诶诶，那个谁，你怎么睡在地上啊。(好听的女声)".colored(), 10, 100, 10)
                 MessageAPI.sendActionTip(player, "&6&l➱ &e提示: 去找樱儿.")
-                player.removePotionEffect(PotionEffectType.CONFUSION)
+                player.removePotionEffect(PotionEffectType.BLINDNESS)
                 EternalNoobie.luckPermsAPI.userManager.also {
                     val user = it.getUser(player.uniqueId)!!
                     user.data().add(PermissionNode.builder("noobie_tutorial").build())
